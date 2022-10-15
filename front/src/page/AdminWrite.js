3d374384082914860cd447a8e005a749110b3ad2
YounmiSon <symym629@gmail.com>
2022-10-15 23:24:31 +0900

    게시판 글쓰기 구현 하는 중

--- front/src/page/AdminWrite.js
+++ front/src/page/AdminWrite.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const AdminWrite = () => {
+  const dispatch = useDispatch;
+  const loginUser = useSelector((state) => state.loginUser);
+  const [inputs, setInputs] = useState({
+    title: "",
+    content: "",
+  });
+  const inputsHandler = (e) => {
+    const { name, value } = e.target;
+    setInputs({ ...inputs, [name]: value });
+  };
+  const submitHandler = () =>
+    dispatch({
+      type: "ADDPOST",
+      payload: { ...inputs },
+    });
+
   return (
     <>
       <div className="flex flex-col justify-center items-center my-8">
         <label>제목</label>
-        <input type="text" className="border-[1px] border-black w-[50%]" />
-        <label className="mt-4">내용</label>
-        <textarea className="border-[1px] border-black w-[50%] h-80" />
         <input
-          type="submit"
-          value="등록하기"
-          className="justify-center items-center bg-yellow-200 p-3 rounded-xl mt-4"
+          type="text"
+          name="title"
+          value={inputs.title}
+          onChange={inputsHandler}
+          className="border-[1px] border-black w-[50%]"
         />
+        <label className="mt-4">내용</label>
+        <textarea
+          name="content"
+          value={inputs.content}
+          onChange={inputsHandler}
+          className="border-[1px] border-black w-[50%] h-80"
+        />
+        <button
+          onClick={submitHandler}
+          className="justify-center items-center bg-yellow-200 p-3 rounded-xl mt-4"
+        >
+          등록하기
+        </button>
       </div>
     </>
   );
