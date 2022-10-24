dd9d79ae18b988b7bdedd723ff71815c928e87e1
YounmiSon <symym629@gmail.com>
2022-10-24 17:21:05 +0900

    마이페이지 포인트 관련 추가

--- front/src/page/Mypage.js
+++ front/src/page/Mypage.js
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 
 const Mypage = () => {
   const nav = useNavigate();
-  // const [user, setUser] = useState();
 
   // 수정하기 누르면 이동시키기
   const editUserInfo = () => {
@@ -15,22 +14,42 @@ const Mypage = () => {
   const nickname = useSelector((state) => state.loginReducer.nickname);
 
   // 포인트 가져오기
+  const point = useSelector((state) => state.loginReducer.point);
 
   // 프로필 사진 수정한 다음 저장
 
   return (
     <>
-      <div className="flex justify-center items-center  mt-28 w-screen">
-        <div className="mx-10">
-          <img className="w-96 my-8 rounded-full" src="/img/default_profile.png" alt="profile" />
+      <div className="flex flex-col justify-center items-center mt-28">
+        <div className="flex justify-center items-center border-[2px] rounded-xl">
+          <div className="mx-10">
+            <img className="w-56 my-6 rounded-full" src="/img/default_profile.png" alt="profile" />
+          </div>
+          <div className="flex flex-col items-start mx-10">
+            <span className="text-xl">닉네임 : {nickname}</span>
+            <span className="text-xl">보유포인트: {point}</span>
+            <div>
+              <input onClick={editUserInfo} type="submit" value="수정하기" className="cursor-pointer bg-indigo-200 ml-8 mt-2 p-3 rounded-xl" />
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col items-center">
-          <span className="text-2xl">닉네임 : {nickname}</span>
-          {/* <span className="text-2xl">보유포인트: {point}</span>/ */}
+        <div>
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="my-8 text-xl">포인트 사용내역</h1>
+            <table className="w-[800px]">
+              <thead>
+                <tr className="grid-rows-3 text-center bg-gray-100 h-12 border-[1px] border-b-black">
+                  <td>상품 이름</td>
+                  <td>사용 포인트</td>
+                  <td>잔여 포인트</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr></tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-      <div className="flex items-center justify-center">
-        <input onClick={editUserInfo} type="submit" value="수정하기" className="cursor-pointer" />
       </div>
     </>
   );
